5f77249143d6ab4ca88a55cc6bebb38f60c8eb08
manuelgsoft <manuel.agm@uma.es>
2025-02-07 22:25:53 +0100

    Add status descriptions, astral chart and astral values

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,5 @@
 ATTRIBUTES = { 0: "body", 1: "mind", 2: "soul" }
+
 STATUS = {
     0: "Hit advantage",
     1: "Hit disadvantage",
@@ -11,6 +12,20 @@ STATUS = {
     8: "Skepticism",
     9: "Idle"
 }
+
+STATUS_DESCRIPTIONS = {
+    0: "For transmutations and arts, you calculate twice to determine if you hit, using the higher result",
+    1: "For transmutations and arts, you calculate twice to determine if you hit, using the lower result",
+    2: "Value added to the hit chances of your transmutations and arts",
+    3: "For transmutations and arts, you calculate twice to determine their effect, using the higher result",
+    4: "For transmutations and arts, you calculate twice to determine their effect, using the lower result",
+    5: "Value added to the calculated outcome of your transmutations and arts.",
+    6: "Your body loses health points over time",
+    7: "Your mind loses health points over time",
+    8: "Your soul loses health points over time",
+    9: "The number of actions per turn for this attribute is reduced"
+}
+
 ENHANCEMENTS = {
     0: "Equipment",
     1: "Philosophy",
@@ -18,7 +33,23 @@ ENHANCEMENTS = {
 }
 
 SPECIAL_ARTS = {
-        0: "Use item",
-        1: "Talk",
-        2: "Commune"
-    }
+    0: "Use item",
+    1: "Talk",
+    2: "Commune"
+}
+
+ASTRAL_VALUES = {
+    -1: "Strong against",
+    0: "Neutral",
+    1: "Weak against"
+}
+
+ASTRAL_CHART = {
+    0: "Mars",
+    1: "Mercury",
+    2: "Jupiter",
+    3: "Saturn",
+    4: "Venus",
+    5: "Moon",
+    6: "Sun"
+}
