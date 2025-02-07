578453215f44648ed8a5376797258dc80bcc82de
manuelgsoft <manuel.agm@uma.es>
2025-02-07 22:27:08 +0100

    Make minor changes to Status.py

--- combat/Status.py
+++ combat/Status.py
@@ -1,21 +1,25 @@
-
+from utils import constants
 
 class Status:
-    def __init__(self, identifier: int, remaining_turns: int, effect: int):
+    def __init__(self, identifier: int, remaining_actions: int, effect: int):
         self.identifier = identifier
-        self.remaining_turns = remaining_turns
+        self.description = constants.STATUS_DESCRIPTIONS[self.identifier]
+        self.remaining_actions = remaining_actions
         self.effect = effect
         self.active = True
 
-    def set_remaining_turns(self, remaining_turns: int):
-        self.remaining_turns = remaining_turns
-        if self.remaining_turns <= 0:
+    def __repr__(self):
+        return f'{constants.STATUS[self.identifier]} +{self.effect} for {self.remaining_actions} actions'
+
+    def set_remaining_actions(self, remaining_actions: int):
+        self.remaining_actions = remaining_actions
+        if self.remaining_actions <= 0:
             self.active = False
 
     def set_effect(self, effect: int):
         self.effect = effect
 
-    def pass_turn(self):
-        self.remaining_turns -= 1
-        if self.remaining_turns <= 0:
+    def pass_action(self):
+        self.remaining_actions -= 1
+        if self.remaining_actions <= 0:
             self.active = False
