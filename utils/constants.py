5fdc76e4de3243a1302bac3e73490e62158f4cf3
manuel-agm <manuel.agm@uma.es>
2025-02-07 09:48:36 +0100

    Add new variables to constants.py

---
+++ utils/constants.py
@@ -0,0 +1,24 @@
+ATTRIBUTES = { 0: "body", 1: "mind", 2: "soul" }
+STATUS = {
+    0: "Hit advantage",
+    1: "Hit disadvantage",
+    2: "Hit modifier",
+    3: "Effect advantage",
+    4: "Effect disadvantage",
+    5: "Effect modifier",
+    6: "Bleeding",
+    7: "Insanity",
+    8: "Skepticism",
+    9: "Idle"
+}
+ENHANCEMENTS = {
+    0: "Equipment",
+    1: "Philosophy",
+    2: "Belief "
+}
+
+SPECIAL_ARTS = {
+        0: "Use item",
+        1: "Talk",
+        2: "Commune"
+    }
